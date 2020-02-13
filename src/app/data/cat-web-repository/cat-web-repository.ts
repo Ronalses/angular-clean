4b020cfd2b30509d1189783840deee119b2293e5
Ronalses Aguilar <ronalses.developer@gmail.com>
2020-02-13 12:04:36 -0300

    data

--- src/app/data/cat-web-repository/cat-web-repository.ts
+++ src/app/data/cat-web-repository/cat-web-repository.ts
@@ -2,8 +2,10 @@ import { Injectable, } from "@angular/core";
 import { CatRepository } from 'src/app/core/repositories/cat.repository';
 import { CatWebRepositoryMapper } from './cat-web-respository-mapper';
 import { Observable } from 'rxjs';
+import { map, flatMap } from 'rxjs/operators';
 import { CatModel } from 'src/app/core/domain/cat.model';
 import {CatWebEntity} from './cat-web-entity';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +13,7 @@ import {CatWebEntity} from './cat-web-entity';
 export default class CatWebRepository extends CatRepository{
     mapper = new CatWebRepositoryMapper();
     
+    baseUrl: string = 'http://5b8d40db7366ab0014a29bfa.mockapi.io/api/v1/elephants'
     constructor(
         private http: HttpClient
     ){
@@ -19,10 +22,14 @@ export default class CatWebRepository extends CatRepository{
 
     getCatById(id: number): Observable<CatModel>{
         return this.http
-            .get<CatWebEntity>(]]http://5b8d40db7366ab0014a29bfa.mockapi.io/api/v1/elephants/${id}'))
+            .get<CatWebEntity>(`${this.baseUrl}/${id}`)
+            .pipe(map(this.mapper.mapFrom));
     }
 
     getAllCats(): Observable<CatModel>{
-
+        return this.http
+            .get<CatWebEntity[]>(`${this.baseUrl}`)
+            .pipe(flatMap(item => item))
+            .pipe(map(this.mapper.mapFrom));
     }
 }
