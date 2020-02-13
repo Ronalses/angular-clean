abcbf5308aba3c98b47d7bab649dfcf1050ee855
Ronalses Aguilar <ronalses.developer@gmail.com>
2020-02-13 12:05:08 -0300

    routing presentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatCardListComponent } from './presentation/cat-card-list/cat-card-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CatCardListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatRepository } from './core/repositories/cat.repository';
+import CatWebRepository from './data/cat-web-repository/cat-web-repository';
+import { PresentationModule } from './presentation/presentation.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PresentationModule
   ],
-  providers: [],
+  providers: [
+    {provide: CatRepository, useClass: CatWebRepository}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
